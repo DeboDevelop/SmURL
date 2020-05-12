38ac9d90fe91918f87edb4912f94014f435f1132
DeboDevelop <debajyotidofficial@gmail.com>
2020-05-12 15:22:19 +0530

    Created teh Model and homepage.

--- src/API/models.py
+++ src/API/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class URL(models.Model):
+    long_url = models.TextField()
+    base62_id = models.CharField(max_length=7)
--- src/SmURL/settings.py
+++ src/SmURL/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'API',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'SmURL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
